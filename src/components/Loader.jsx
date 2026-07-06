4645b357fe9a3d1955a486b6ac6fdd2bd6c81762
Tuyisenge Esther <tuyisesther2020@gmail.com>
2026-07-06 22:10:48 +0200

    Fix loader default export

---
+++ src/components/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="flex justify-center items-center py-20">
+      <p className="text-gray-600 text-lg">Loading...</p>
+    </div>
+  );
+}
+
+export default Loader;
